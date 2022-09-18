27b2b4e4a54fce4815f1c65c6b5969df83bf77b2
SALT0S <josesansalt@gmail.com>
2022-09-17 19:21:18 -0500

    To production

--- database/db.ts
+++ database/db.ts
@@ -33,7 +33,7 @@ export const connect = async () => {
 };
 
 export const disconnect = async () => {
-  if (process.env.NODE_ENV === "development") return;
+  if (process.env.NODE_ENV === "production") return;
 
   if (mongooConnection.isConnected === 0) return;
 
